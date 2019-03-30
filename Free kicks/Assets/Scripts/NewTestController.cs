49d3c41e43e6ed675aacb8f52752e1b1bec44c96
chrisliddell <chrisliddell78958@gmail.com>
2019-03-30 13:40:25 -0600

    test saving module for new test

--- Free kicks/Assets/Scripts/NewTestController.cs
+++ Free kicks/Assets/Scripts/NewTestController.cs
@@ -1,21 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewTestController : MonoBehaviour
 {
 	public GameObject mainMenu;
+	public string test, quest;
+	public int index;
+	public GameObject[] options;
+	public GameObject testName;
+	public GameObject question;
+	public GameObject buttonAdd;
+	public GameObject buttonFinish;
+	public GameObject questionsPanels;
+	GameObject input;
     // Start is called before the first frame update
     void Start()
     {
-        
+        test = "";
+		quest = "";
+		index = PlayerPrefs.GetInt("index", 0);
+		index++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(dataValid())
+			buttonAdd.GetComponent<Button>().interactable = true;
+		else 
+			buttonAdd.GetComponent<Button>().interactable = false;
+		
+		if(test == "")
+			buttonFinish.GetComponent<Button>().interactable = false;
+		else
+			buttonFinish.GetComponent<Button>().interactable = true;
+	}
+	
+	public bool dataValid(){
+		if(testName.GetComponent<Text>().text != ""){
+			if(question.GetComponent<Text>().text != ""){
+				if(options[0].GetComponent<Text>().text != ""){
+					if(options[1].GetComponent<Text>().text != ""){
+						if(options[2].GetComponent<Text>().text != ""){
+							if(options[3].GetComponent<Text>().text != ""){
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
 	
 	public void show(){
 		mainMenu.SetActive(false);
@@ -28,7 +66,53 @@ public class NewTestController : MonoBehaviour
 	}
 	
 	public void Cancel(){
+		test = "";
+		questionsPanels.GetComponent<Text>().text = test;
+		clearTextFields();
 		hide();
 	}
 	
+	public void addQuestion(){
+		quest = question.GetComponent<Text>().text + ": [" + options[0].GetComponent<Text>().text + ", " + options[1].GetComponent<Text>().text + ", " + options[2].GetComponent<Text>().text + ", " + options[3].GetComponent<Text>().text+"]";
+		questionsPanels.GetComponent<Text>().text += quest + "\n";
+		test += "\n\t{\n\t\t" + quest + "\n\t}";
+		clearQuestion();
+	}
+	
+	public void clearTextFields(){
+		input = GameObject.Find("TestName");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("Question");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("OptionA");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("OptionB");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("OptionC");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("OptionD");
+		input.GetComponent<InputField>().text = "";
+	}
+	
+	public void clearQuestion(){
+		input = GameObject.Find("Question");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("OptionA");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("OptionB");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("OptionC");
+		input.GetComponent<InputField>().text = "";
+		input = GameObject.Find("OptionD");
+		input.GetComponent<InputField>().text = "";
+	}
+	
+	
+	public void save(){
+		test = "{"+ testName.GetComponent<Text>().text +":"+test+"\n}";
+		clearTextFields();
+		Debug.Log(test);
+		PlayerPrefs.SetString(index+"", test);
+		test = "";
+	}
 }
